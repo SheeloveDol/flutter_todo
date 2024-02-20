b1ea15a3dd0c736a2708e3e446a3c9fdf7fa3e6c
Sheelove Dol <siesel26@gmail.com>
2024-02-19 21:22:52 -0800

    Added riverpod for state management and configured home page to show added todos

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,30 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:todo_app/models/todo.dart';
 import 'package:todo_app/pages/add.dart';
+import 'package:todo_app/providers/todo_provider.dart';
 
-class MyHomePage extends StatefulWidget {
+// This is the homepage of the app. To consume the state, we need to use the ConsumerWidget and import the flutter_riverpod package
+class MyHomePage extends ConsumerWidget {
   const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  // The build method is where the UI is built and the state is consumed using the ref parameter
+  Widget build(BuildContext context, WidgetRef ref) {
+    List<Todo> todos = ref.watch(
+        todoProvider); // Read the state of the todoProvider and gets the type from the models/todo.dart file
 
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Todo App"),
         centerTitle: true,
       ),
-      body: const Center(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Navigate to the AddTodo page
-          Navigator.of(context)
-              .push(MaterialPageRoute(builder: (context) => const AddTodo()));
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: ListView.builder(
+          itemCount: todos.length,
+          itemBuilder: (context, index) {
+            return Slidable(
+                startActionPane: ActionPane(
+                  motion: const ScrollMotion(),
+                  children: [
+                    SlidableAction(onPressed: (context) => 
+                      ref.watch(todoProvider.notifier).deleteTodo(index),
+                    icon: Icons.delete,
+                    backgroundColor: Colors.red,
+                    borderRadius: const BorderRadius.all(Radius.circular(18)),
+                    )
+                    ],
+                ),
+                endActionPane: ActionPane(
+                  motion: const ScrollMotion(),
+                  children: [
+                    SlidableAction(onPressed: (context) => 
+                      ref.watch(todoProvider.notifier).completeTodo(index),
+                    icon: Icons.check,
+                    backgroundColor: Colors.green,
+                    borderRadius: const BorderRadius.all(Radius.circular(18)),
+                    )
+                    ],
+                ),
+                child: ListTile(
+                  title: Text(todos[index].content),
+                ));
+          }),
+      floatingActionButton: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate to the AddTodo page
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (context) => const AddTodo()));
+          },
+          child: const Text("Add a Todo"),
+        ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
